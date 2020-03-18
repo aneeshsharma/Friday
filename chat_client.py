7136aec1202e0851bec3285ab71f081b39c42c79
Anish Sharma <aneeshsharma15@outlook.com>
2020-03-18 21:49:41 +0530

    Changed the Header format

--- chat_client.py
+++ chat_client.py
@@ -1,5 +1,5 @@
 import socket
-from socket_connection import *
+from socket_connection import Socket
 
 ip = input('IP: ')
 port = int(input('Port: '))
@@ -9,17 +9,17 @@ sock = Socket()
 sock.connect(ip, port)
 
 while True:
-	data = input('You:\t')
-	try:
-		sock.send(data)
-	except:
-		print('Disconnected')
-		break
-	try:
-		recv = sock.recv()
-	except:
-		print('Disconnected')
-		break
-	print('\033[1;34mFriday:\t', recv.decode('utf-8') + '\033[0;37m')
+    data = input('You:\t')
+    try:
+        sock.send(data)
+    except:
+        print('Disconnected')
+        break
+    try:
+        msg, data_type = sock.recv()
+    except:
+        print('Disconnected')
+        break
+    print('\033[1;34mFriday:\t', msg.decode('utf-8') + '\033[0;37m')
 
 sock.close()
--- friday_central.py
+++ friday_central.py
@@ -3,13 +3,14 @@ import socket_connection
 import threading
 import eliza
 
+
 def eliza_bot(sock, addr):
     bot = eliza.eliza()
     print('Eliza : Connection Received -', sock.getpeername())
     data = b''
     while data != b'exit':
         try:
-            data = socket_connection.receive_pack(sock)
+            data = socket_connection.receive_pack(sock)[0]
             if data == b'exit':
                 print('Eliza : Exit -', sock.getpeername())
                 break
@@ -17,10 +18,35 @@ def eliza_bot(sock, addr):
             print('Eliza : Disconnected -', sock.getpeername())
             break
         print('Eliza : Data Received: ', data)
-        reply = ('Eliza : ' + bot.respond(data.decode('utf-8'))).encode('utf-8')
+        reply = ('Eliza:' + bot.respond(data.decode('utf-8'))).encode('utf-8')
         socket_connection.send_pack(sock, reply)
     sock.close()
 
-command_server = threading.Thread(target=socket_server.socket_server, args=(eliza_bot,))
+
+def discord_bot(sock, addr):
+    bot = eliza.eliza()
+    print('Eliza : Connection Received -', sock.getpeername())
+    data = b''
+    while data != b'exit':
+        try:
+            data = socket_connection.receive_pack(sock)[0]
+            if data == b'exit':
+                print('Eliza : Exit -', sock.getpeername())
+                break
+        except:
+            print('Eliza : Disconnected -', sock.getpeername())
+            break
+        print('Eliza : Data Received: ', data)
+        reply = (bot.respond(data.decode('utf-8'))).encode('utf-8')
+        socket_connection.send_pack(sock, reply)
+    sock.close()
+
+
+command_server = threading.Thread(
+    target=socket_server.socket_server, args=(eliza_bot,))
+
+discord_bot_server = threading.Thread(
+    target=socket_server.socket_server, args=(discord_bot,))
 
 command_server.start()
+discord_bot_server.start()
